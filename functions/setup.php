94ffd1908554b5f99ee3f161ade46eb9cc83b082
Simon Padbury <simon_padbury@yahoo.co.uk>
2016-02-03 09:06:49 +0000

    Update setup.php

--- functions/setup.php
+++ functions/setup.php
@@ -17,44 +17,6 @@ function b4st_excerpt_readmore() {
 }
 add_filter('excerpt_more', 'b4st_excerpt_readmore');
 
-// Browser detection body_class() output
-
-function b4st_browser_body_class( $classes ) {
-	global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
-	
-	if($is_lynx) $classes[] = 'lynx';
-	elseif($is_gecko) $classes[] = 'gecko';
-	elseif($is_opera) $classes[] = 'opera';
-	elseif($is_NS4) $classes[] = 'ns4';
-	elseif($is_safari) $classes[] = 'safari';
-	elseif($is_chrome) $classes[] = 'chrome';
-	elseif($is_IE) {
-		$browser = $_SERVER['HTTP_USER_AGENT'];
-		$browser = sub4str( "$browser", 25, 8);
-		if ($browser == "MSIE 7.0"  ) {
-			$classes[] = 'ie7';
-			$classes[] = 'ie';
-		} elseif ($browser == "MSIE 6.0" ) {
-			$classes[] = 'ie6';
-			$classes[] = 'ie';
-		} elseif ($browser == "MSIE 8.0" ) {
-			$classes[] = 'ie8';
-			$classes[] = 'ie';
-		} elseif ($browser == "MSIE 9.0" ) {
-			$classes[] = 'ie9';
-			$classes[] = 'ie';
-		} else {
-	            $classes[] = 'ie';
-	        }
-	}
-	else $classes[] = 'unknown';
- 
-	if( $is_iphone ) $classes[] = 'iphone';
- 
-	return $classes;
-}
-add_filter( 'body_class', 'b4st_browser_body_class' );
-
 // Add post formats support. See http://codex.wordpress.org/Post_Formats
 add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
 
