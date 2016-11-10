259b20010adc14374b0d73a1a6c133432913ecf2
Simon Padbury <simon_padbury@yahoo.co.uk>
2016-11-10 07:30:38 +0000

    Update float-sm-right

--- navbar-search.php
+++ navbar-search.php
@@ -7,7 +7,7 @@ You can then insert the Search Widget into the sidebar.
 */
 ?>
 
-<form class="form-inline navbar-form pull-right" role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form class="form-inline navbar-form float-sm-right" role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
   <input class="form-control" type="text" value="<?php echo get_search_query(); ?>" placeholder="Search..." name="s" id="s">
-	<button type="submit" id="searchsubmit" value="<?php esc_attr_x('Search', 'b4st') ?>" class="btn btn-primary active"><i class="fa fa-search"></i></button>
+  <button type="submit" id="searchsubmit" value="<?php esc_attr_x('Search', 'b4st') ?>" class="btn btn-primary active"><i class="fa fa-search"></i></button>
 </form>
