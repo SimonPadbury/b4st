71c04b6c9ee662b9f7d101b62a9bb48493bbb504
Wp Dev <indteamhost@gmail.com>
2016-12-02 22:21:28 +0700

    update function.php
    Replace with get_template_directory new core wordpress use this

--- functions.php
+++ functions.php
@@ -3,12 +3,12 @@
 All the functions are in the PHP pages in the `functions/` folder.
 */
 
-require_once locate_template('/functions/cleanup.php');
-require_once locate_template('/functions/setup.php');
-require_once locate_template('/functions/enqueues.php');
-require_once locate_template('/functions/navbar.php');
-require_once locate_template('/functions/widgets.php');
-require_once locate_template('/functions/search-widget.php');
-require_once locate_template('/functions/index-pagination.php');
-require_once locate_template('/functions/split-post-pagination.php');
-require_once locate_template('/functions/feedback.php');
+	require get_template_directory() . '/functions/cleanup.php';
+	require get_template_directory() . '/functions/setup.php';
+	require get_template_directory() . '/functions/enqueues.php';
+	require get_template_directory() . '/functions/navbar.php';
+	require get_template_directory() . '/functions/widgets.php';
+	require get_template_directory() . '/functions/search-widget.php';
+	require get_template_directory() . '/functions/index-pagination.php';
+	require get_template_directory() . '/functions/split-post-pagination.php';
+	require get_template_directory() . '/functions/feedback.php';
