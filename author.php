08b7918ef96f510251bfe2a4d72b9f4f3feafae6
Kevin Johnson <kevin@amplitudedesign.com>
2017-05-19 09:05:57 -0500

    Check if author has posts before loading content template. Change the code used to display the author name.

--- author.php
+++ author.php
@@ -7,18 +7,20 @@
 
 			<header class="page-header">
 				<h1>
-                    <?php echo sprintf(__('Author %s', 'b4st'), '<strong>' . get_the_author() . '</strong>'); ?>
+                    <?php echo sprintf(__('Author %s', 'b4st'), '<strong>' . get_the_author_meta('user_nicename', get_query_var('author')) . '</strong>'); ?>
 				</h1>
 			</header><!-- .page-header -->
-
+            <?php if(have_posts()): ?>
                <?php get_template_part('loops/content', get_post_format()); ?>
-			
+            <?php else: ?>
+               <?php get_template_part('loops/content', 'none'); ?>
+            <?php endif; ?>
 		</div>
 
 		<div class="col-sm-4" id="sidebar">
 		   <?php get_sidebar(); ?>
 		</div>
-		
+
 	  </div><!-- /.row -->
 	</div><!-- /.container -->
 
