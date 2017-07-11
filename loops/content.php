71719960bc162fd1718d8b998dbab5c08043f930
Simon Padbury <simon.padbury@icloud.com>
2017-07-11 14:44:00 +0100

    Update content.php

--- loops/content.php
+++ loops/content.php
@@ -37,8 +37,8 @@ Alternatively, notice that index.php, category.php and single.php have a post_cl
 
 <?php if ( function_exists('b4st_pagination') ) { b4st_pagination(); } else if ( is_paged() ) { ?>
   <ul class="pagination">
-    <li class="older"><?php next_posts_link('<i class="fa fa-arrow-left"></i> ' . __('Previous', 'b4st')) ?></li>
-    <li class="newer"><?php previous_posts_link(__('Next', 'b4st') . ' <i class="fa fa-arrow-right"></i>') ?></li>
+    <li class="page-item older"><?php next_posts_link('<i class="fa fa-arrow-left"></i> ' . __('Previous', 'b4st')) ?></li>
+    <li class="page-item newer"><?php previous_posts_link(__('Next', 'b4st') . ' <i class="fa fa-arrow-right"></i>') ?></li>
   </ul>
 <?php } ?>
 
