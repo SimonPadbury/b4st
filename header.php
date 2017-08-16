7316c65913982bcc24f2b6ca1dc6fac33d27ce93
Simon Padbury <simon.padbury@icloud.com>
2017-08-16 12:57:15 +0100

    Updated navbar for Bootstrap 4.0.0-beta

--- header.php
+++ header.php
@@ -10,23 +10,23 @@
 
 <body <?php body_class(); ?>>
 
-<nav class="navbar navbar-toggleable-md navbar-inverse bg-primary">
+<nav class="navbar navbar-expand-md navbar-inverse bg-primary">
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <a class="navbar-brand" href="<?php echo esc_url( home_url('/') ); ?>"><?php bloginfo('name'); ?></a>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
-		<?php
+    <?php
       wp_nav_menu( array(
-        'theme_location'		=> 'navbar',
-        'container'         => false,
-        'menu_class'				=> '',
-        'fallback_cb'				=> '__return_false',
-      	'items_wrap'				=> '<ul id="%1$s" class="navbar-nav mr-auto mt-2 mt-lg-0 %2$s">%3$s</ul>',
-        'depth'							=> 2,
-				'walker'            => new b4st_walker_nav_menu()
+        'theme_location'	=> 'navbar',
+        'container'         	=> false,
+        'menu_class'		=> '',
+        'fallback_cb'		=> '__return_false',
+      	'items_wrap'		=> '<ul id="%1$s" class="navbar-nav mr-auto mt-2 mt-lg-0 %2$s">%3$s</ul>',
+        'depth'			=> 2,
+	'walker'  	        => new b4st_walker_nav_menu()
       ) );
     ?>
-		<?php get_template_part('navbar-search'); ?>
+    <?php get_template_part('navbar-search'); ?>
   </div>
 </nav>
