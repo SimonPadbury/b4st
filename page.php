c42c83021fc8338083dc604f6a160627acfda2bc
Simon Padbury <simon.padbury@icloud.com>
2017-12-21 10:24:02 +0000

    Update page.php

--- page.php
+++ page.php
@@ -2,17 +2,17 @@
 
 <div class="container">
   <div class="row">
-    
+
     <div class="<?php if(is_active_sidebar('sidebar-widget-area')): ?>col-sm-8<?php else: ?>col-sm-12<?php endif; ?>">
       <div id="content" role="main">
-        <?php get_template_part('loops/content', 'page'); ?>
+        <?php get_template_part('loops/page-content'); ?>
       </div><!-- /#content -->
     </div>
-    
+
     <div class="col-sm-4" id="sidebar" role="navigation">
        <?php get_sidebar(); ?>
     </div>
-    
+
   </div><!-- /.row -->
 </div><!-- /.container -->
 
