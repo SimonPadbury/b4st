451a6975bd79d6079b11f3cff47d7dff57b6290f
Simon Padbury <simon.padbury@icloud.com>
2017-12-21 17:21:19 +0000

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html class="no-js">
 <head>
-	<title><?php wp_title('•', true, 'right'); bloginfo('name'); ?></title>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
- 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<?php wp_head(); ?>
+  <title><?php wp_title('•', true, 'right'); bloginfo('name'); ?></title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -21,13 +21,13 @@
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <?php
       wp_nav_menu( array(
-        'theme_location'	=> 'navbar',
+        'theme_location'  => 'navbar',
         'container'       => false,
-        'menu_class'		  => '',
-        'fallback_cb'		  => '__return_false',
-      	'items_wrap'		  => '<ul id="%1$s" class="navbar-nav mr-auto mt-2 mt-lg-0 %2$s">%3$s</ul>',
-        'depth'			      => 2,
-	      'walker'  	      => new b4st_walker_nav_menu()
+        'menu_class'      => '',
+        'fallback_cb'     => '__return_false',
+        'items_wrap'      => '<ul id="%1$s" class="navbar-nav mr-auto mt-2 mt-lg-0 %2$s">%3$s</ul>',
+        'depth'           => 2,
+        'walker'          => new b4st_walker_nav_menu()
       ) );
     ?>
     <?php get_template_part('navbar-search'); ?>
