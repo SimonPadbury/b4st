a2535abe44c0812ab2a3637ca0678c91b415cf8d
Simon Padbury <simon.padbury@icloud.com>
2017-12-21 17:19:41 +0000

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<title><?php wp_title('•', true, 'right'); bloginfo('name'); ?></title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php wp_head(); ?>
 </head>
 
