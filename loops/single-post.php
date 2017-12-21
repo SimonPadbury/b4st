1c00640b94142792fc8d899967d45e47271e70e0
Simon Padbury <simon.padbury@icloud.com>
2017-12-21 10:18:15 +0000

    Update and rename content-single.php to single-post.php

--- loops/single-post.php
+++ loops/single-post.php
@@ -8,9 +8,9 @@ The Single Posts Loop
   <?php if(have_posts()): while(have_posts()): the_post(); ?>
   <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
     <header>
-      <h2>
+      <h1>
         <?php the_title()?>
-      </h2>
+      </h1>
       <h5>
         <em>
           <span class="text-muted author"><?php _e('By', 'b4st'); echo " "; the_author() ?>,</span>
@@ -18,10 +18,10 @@ The Single Posts Loop
         </em>
       </h5>
       <p class="text-muted" style="margin-bottom: 30px;">
-        <i class="fas fa-folder-open"></i>&nbsp;
+        <i class="fa fa-folder-open"></i>&nbsp;
         <?php _e('Filed under', 'b4st'); ?>:
         <?php the_category(', ') ?><br/>
-        <i class="fas fa-comment"></i>&nbsp;
+        <i class="fa fa-comment"></i>&nbsp;
         <?php _e('Comments', 'b4st'); ?>:
         <?php comments_popup_link(__('None', 'b4st'), '1', '%'); ?>
       </p>
