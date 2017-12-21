fa311592fecd1250b772861fd091b6c59a68213d
Simon Padbury <simon.padbury@icloud.com>
2017-12-21 16:58:08 +0000

    Update index-loop.php

--- loops/index-loop.php
+++ loops/index-loop.php
@@ -16,9 +16,9 @@ If you require only post excerpts to be shown in index and category pages, then 
 <?php if ( function_exists('b4st_pagination') ) { b4st_pagination(); } else if ( is_paged() ) { ?>
 <ul class="pagination">
   <li class="page-item older">
-    <?php next_posts_link('<i class="fa fa-arrow-left"></i> ' . __('Previous', 'b4st')) ?></li>
+    <?php next_posts_link('<i class="fas fa-arrow-left"></i> ' . __('Previous', 'b4st')) ?></li>
   <li class="page-item newer">
-    <?php previous_posts_link(__('Next', 'b4st') . ' <i class="fa fa-arrow-right"></i>') ?></li>
+    <?php previous_posts_link(__('Next', 'b4st') . ' <i class="fas fa-arrow-right"></i>') ?></li>
 </ul>
 <?php } ?>
 
