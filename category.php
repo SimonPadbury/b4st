3df83380ff4421c6e58b5fea91cfb8eb3150d8dd
Simon Padbury <simon.padbury@icloud.com>
2017-12-21 10:23:33 +0000

    Update category.php

--- category.php
+++ category.php
@@ -2,21 +2,22 @@
 
 <div class="container">
   <div class="row">
-    
+
     <div class="<?php if(is_active_sidebar('sidebar-widget-area')): ?>col-sm-8<?php else: ?>col-sm-12<?php endif; ?>">
+
       <div id="content" role="main">
         <header>
           <h1>Category: <?php echo single_cat_title(); ?></h1>
           <hr>
         </header>
-        <?php get_template_part('loops/content', get_post_format()); ?>
+        <?php get_template_part('loops/index-loop'); ?>
       </div><!-- /#content -->
     </div>
-    
+
     <div class="col-sm-4" id="sidebar" role="navigation">
        <?php get_sidebar(); ?>
     </div>
-    
+
   </div><!-- /.row -->
 </div><!-- /.container -->
 
