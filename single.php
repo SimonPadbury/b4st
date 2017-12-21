ccf76132cbd4fa7af3cd6f53c5b11db7d3e2836d
Simon Padbury <simon.padbury@icloud.com>
2017-12-21 10:22:32 +0000

    Update single.php

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
     
     <div class="<?php if(is_active_sidebar('sidebar-widget-area')): ?>col-sm-8<?php else: ?>col-sm-12<?php endif; ?>">
       <div id="content" role="main">
-        <?php get_template_part('loops/content-single', 'single'); ?>
+        <?php get_template_part('loops/single-post', get_post_format()); ?>
       </div><!-- /#content -->
     </div>
     
