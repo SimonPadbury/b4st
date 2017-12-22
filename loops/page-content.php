7144725b0af4c539b4d58ec3565ac28d369ca57b
Simon Padbury <simon.padbury@icloud.com>
2017-12-22 19:38:29 +0000

    Update page-content.php

--- loops/page-content.php
+++ loops/page-content.php
@@ -8,7 +8,7 @@ The Page Content Loop
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
   <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
     <header>
-      <h2><?php the_title()?></h2>
+      <h1><?php the_title()?></h1>
     </header>
     <?php the_content()?>
     <?php wp_link_pages(); ?>
