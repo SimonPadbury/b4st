1ad0e54832c87609b191b39fd700ee27d5c8fb16
Simon Padbury <simon.padbury@icloud.com>
2017-12-29 15:53:54 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
 <?php
 /*
-All the functions are in the PHP pages in the `functions/` folder.
+All the functions are in the PHP files in the `functions/` folder.
 */
 
-	require get_template_directory() . '/functions/cleanup.php';
-	require get_template_directory() . '/functions/setup.php';
-	require get_template_directory() . '/functions/enqueues.php';
-	require get_template_directory() . '/functions/navbar.php';
-	require get_template_directory() . '/functions/widgets.php';
-	require get_template_directory() . '/functions/search-widget.php';
-	require get_template_directory() . '/functions/index-pagination.php';
-	require get_template_directory() . '/functions/split-post-pagination.php';
-	require get_template_directory() . '/functions/feedback.php';
-	require get_template_directory() . '/functions/remove-query-string.php';
+require get_template_directory() . '/functions/cleanup.php';
+require get_template_directory() . '/functions/setup.php';
+require get_template_directory() . '/functions/enqueues.php';
+require get_template_directory() . '/functions/navbar.php';
+require get_template_directory() . '/functions/widgets.php';
+require get_template_directory() . '/functions/search-widget.php';
+require get_template_directory() . '/functions/index-pagination.php';
+require get_template_directory() . '/functions/split-post-pagination.php';
+require get_template_directory() . '/functions/feedback.php';
+require get_template_directory() . '/functions/remove-query-string.php';
