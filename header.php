bd8efeccba9ddc47adb48421caf91127dc7215ab
Simon Padbury <simon.padbury@icloud.com>
2018-01-04 18:17:02 +0000

    Update header.php

--- header.php
+++ header.php
@@ -17,32 +17,34 @@
 <body <?php body_class(); ?>>
 
 <nav class="navbar navbar-expand-md navbar-light bg-light">
+  <div class="container">
 
-  <a class="navbar-brand" href="<?php echo esc_url( home_url('/') ); ?>"><?php bloginfo('name'); ?></a>
+    <a class="navbar-brand" href="<?php echo esc_url( home_url('/') ); ?>"><?php bloginfo('name'); ?></a>
 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown" aria-controls="navbarDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown" aria-controls="navbarDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarDropdown">
-    <?php
-      wp_nav_menu( array(
-        'theme_location'  => 'navbar',
-        'container'       => false,
-        'menu_class'      => '',
-        'fallback_cb'     => '__return_false',
-        'items_wrap'      => '<ul id="%1$s" class="navbar-nav mr-auto mt-2 mt-lg-0 %2$s">%3$s</ul>',
-        'depth'           => 2,
-        'walker'          => new b4st_walker_nav_menu()
-      ) );
-    ?>
-    
-    <form class="form-inline mx-0 ml-auto" role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-      <input class="form-control mr-sm-1" type="text" value="<?php echo get_search_query(); ?>" placeholder="Search..." name="s" id="s">
-      <button type="submit" id="searchsubmit" value="<?php esc_attr_x('Search', 'b4st') ?>" class="btn btn-outline-secondary my-2 my-sm-0">
-        <i class="fas fa-search"></i>
-      </button>
-    </form>
+    <div class="collapse navbar-collapse" id="navbarDropdown">
+      <?php
+        wp_nav_menu( array(
+          'theme_location'  => 'navbar',
+          'container'       => false,
+          'menu_class'      => '',
+          'fallback_cb'     => '__return_false',
+          'items_wrap'      => '<ul id="%1$s" class="navbar-nav mr-auto mt-2 mt-lg-0 %2$s">%3$s</ul>',
+          'depth'           => 2,
+          'walker'          => new b4st_walker_nav_menu()
+        ) );
+      ?>
+
+      <form class="form-inline mx-0 ml-auto" role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+        <input class="form-control mr-sm-1" type="text" value="<?php echo get_search_query(); ?>" placeholder="Search..." name="s" id="s">
+        <button type="submit" id="searchsubmit" value="<?php esc_attr_x('Search', 'b4st') ?>" class="btn btn-outline-secondary my-2 my-sm-0">
+          <i class="fas fa-search"></i>
+        </button>
+      </form>
+    </div>
     
   </div>
 </nav>
