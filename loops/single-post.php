ebfcebe3ca7799b86e0abcb4f5abfa6c3916ef39
Simon Padbury <simon.padbury@icloud.com>
2018-01-29 07:44:43 +0000

    Update single-post.php

--- loops/single-post.php
+++ loops/single-post.php
@@ -6,11 +6,11 @@
 
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
   <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
-    <header class="mb-4 border-bottom pb-3">
+    <header class="mb-4">
       <h1>
         <?php the_title()?>
       </h1>
-      <div class="header-meta clearfix">
+      <div class="header-meta text-muted">
         <?php
           _e('By ', 'b4st');
           the_author_posts_link();
@@ -19,36 +19,26 @@
         ?>
       </div>
     </header>
-    <main class="row">
-      <div class="col-md order-md-2">
-        <?php
-          the_post_thumbnail();
-          the_content();
-          wp_link_pages();
-        ?>
-      </div>
-      <div class="col-md-4 order-md-1 text-muted">
-        <i class="far fa-folder-open"></i>&nbsp;
-        <?php _e('Category: ', 'b4st'); the_category(', ') ?><br/>
-        <?php if ( has_tag() ) {
-          ?>
-          <i class="far fa-tags"></i>&nbsp;
-          <?php the_tags('Tags: ', ', '); ?><br/>
-        <?php } ?>
-        <i class="far fa-comment"></i>&nbsp;
-        <?php _e('Comments', 'b4st'); ?>:
-        <?php comments_popup_link(__('None', 'b4st'), '1', '%'); ?><br/>
-
-        <div class="author-bio row mt-4">
-          <div class="col-3 col-md-12 mb-3"><?php b4st_author_avatar(); ?></div>
-          <div class="col-9 col-md-12">
-            <p class="h4 author-name"><?php the_author_posts_link(); ?></p>
-            <p class="author-description"><?php b4st_author_description(); ?></p>
-            <p class="author-other-posts mb-0 border-top pt-3"><?php _e('Other posts by ', 'b4st'); the_author_posts_link(); ?></p>
-          </div>
-        </div><!-- /.author-bio -->
-      </div>
+    <main>
+      <?php
+        the_post_thumbnail();
+        the_content();
+        wp_link_pages();
+      ?>
     </main>
+    <footer class="mt-5 border-top pt-3">
+      <p>
+        <?php _e('Category: ', 'b4st'); the_category(', ') ?> | <?php if (has_tag()) { the_tags('Tags: ', ', '); ?> | <?php } _e('Comments', 'b4st'); ?>: <?php comments_popup_link(__('None', 'b4st'), '1', '%'); ?>
+      </p>
+      <div class="author-bio media border-top pt-3">
+        <?php b4st_author_avatar(); ?>
+        <div class="media-body ml-3">
+          <p class="h4 author-name"><?php the_author_posts_link(); ?></p>
+          <p class="author-description"><?php b4st_author_description(); ?></p>
+          <p class="author-other-posts mb-0 border-top pt-3"><?php _e('Other posts by ', 'b4st'); the_author_posts_link(); ?></p>
+        </div>
+      </div><!-- /.author-bio -->
+    </footer>
   </article>
 <?php
     if ( comments_open() || get_comments_number() ) :
