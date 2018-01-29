350584481fda78d5c79e70d8be6b6f2ca550a1ed
Simon Padbury <simon.padbury@icloud.com>
2018-01-29 07:46:50 +0000

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -4,11 +4,13 @@
  * Note: The main column has simply Bootstrap flexbox "col-sm" so it will expand
  * to occupy the whole row (if no sidebar) or to occupy whatever part of the row
  * is available (if there is a sidebar, or more than one sidebar, etc.).
+ *
+ * (So, you don't need to set the main column to col-sm-8 or whatever.)
  */
 ?>
 
 <?php if( is_active_sidebar('sidebar-widget-area') ): ?>
-<div class="sidebar col-sm-3" id="sidebar" role="navigation">
+<div class="sidebar col-sm-4" id="sidebar" role="navigation">
   <?php dynamic_sidebar('sidebar-widget-area'); ?>
 </div>
 <?php endif; ?>
