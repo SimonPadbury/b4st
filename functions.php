fca6f060673fc473adbb7a453f2946fc741fab23
Simon Padbury <simon.padbury@icloud.com>
2018-03-01 06:50:03 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 All the functions are in the PHP files in the `functions/` folder.
 */
 
-require get_template_directory() . '/functions/cleanup.php';
+//require get_template_directory() . '/functions/cleanup.php';
 require get_template_directory() . '/functions/setup.php';
 require get_template_directory() . '/functions/enqueues.php';
 require get_template_directory() . '/functions/navbar.php';
