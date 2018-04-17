ff55be815eb8ceba48c1f2183b3fadb7687c8379
Simon Padbury <simon.padbury@icloud.com>
2018-04-17 15:48:19 +0100

    Update index-post.php

--- loops/index-post.php
+++ loops/index-post.php
@@ -16,19 +16,19 @@ Used by index.php, category.php and author.php
     <p class="text-muted">
       <i class="far fa-calendar-alt"></i>&nbsp;<?php b4st_post_date(); ?>&nbsp;|
       <i class="far fa-user"></i>&nbsp; <?php _e('By ', 'b4st'); the_author_posts_link(); ?>&nbsp;|
-      <i class="far fa-comment"></i>&nbsp;<a href="<?php comments_link(); ?>"><?php comments_number('No comments', '1 comment', '% comments'); ?></a>
+      <i class="far fa-comment"></i>&nbsp;<a href="<?php comments_link(); ?>"><?php printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), '', 'b4st' ), number_format_i18n( get_comments_number() ) ); ?></a>
     </p>
   </header>
   <main>
     <?php the_post_thumbnail(); ?>
 
     <?php if ( has_excerpt( $post->ID ) ) {
-  		the_excerpt();
+  	the_excerpt();
     ?><p><a href="<?php the_permalink(); ?>">
     	<?php _e( '&hellip; ' . __('Continue reading', 'b4st' ) . ' <i class="fas fa-arrow-right"></i>', 'b4st' ) ?>
       </a></p>
   	<?php } else {
-  		the_content( __( '&hellip; ' . __('Continue reading', 'b4st' ) . ' <i class="fas fa-arrow-right"></i>', 'b4st' ) );
-		} ?>
+  	 the_content( __( '&hellip; ' . __('Continue reading', 'b4st' ) . ' <i class="fas fa-arrow-right"></i>', 'b4st' ) );
+	} ?>
   </main>
 </article>
