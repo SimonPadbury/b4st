54474e9a11787401d290597620da40d7b87c4467
Simon Padbury <simon.padbury@icloud.com>
2018-08-20 06:25:40 +0100

    Update enqueues.php

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -11,16 +11,13 @@ if ( ! function_exists('b4st_enqueues') ) {
 		wp_register_style('bootstrap-css', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css', false, '4.1.3', null);
 		wp_enqueue_style('bootstrap-css');
 
+		wp_register_style('fontawesome5-css', 'https://use.fontawesome.com/releases/v5.2.0/css/all.css', false, '5.2.0', null);
+		wp_enqueue_style('fontawesome5-css');
+
 		wp_register_style('b4st-css', get_template_directory_uri() . '/theme/css/b4st.css', false, null);
 		wp_enqueue_style('b4st-css');
 
 		// Scripts
-
-		wp_register_script('font-awesome-config-js', get_template_directory_uri() . '/theme/js/font-awesome-config.js', false, null, null);
-		wp_enqueue_script('font-awesome-config-js');
-
-		wp_register_script('font-awesome', 'https://use.fontawesome.com/releases/v5.2.0/css/all.css', false, '5.2.0', null);
-		wp_enqueue_script('font-awesome');
 
 		wp_register_script('modernizr',  'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', false, '2.8.3', true);
 		wp_enqueue_script('modernizr');
