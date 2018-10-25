4cb6b7d3383a169a5efd42fc9a00e43978aa1a57
Simon Padbury <simon.padbury@icloud.com>
2018-10-25 14:56:45 +0100

    Update enqueues.php

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -22,8 +22,7 @@ if ( ! function_exists('b4st_enqueues') ) {
 		wp_register_script('modernizr',  'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', false, '2.8.3', true);
 		wp_enqueue_script('modernizr');
 
-		wp_register_script('jquery-3.3.1', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js', false, '3.3.1', true);
-		wp_enqueue_script('jquery-3.3.1');
+		wp_enqueue_script('jquery');
 
 		wp_register_script('popper',  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', false, '1.14.3', true);
 		wp_enqueue_script('popper');
