e5060e0ecdc74aa9486aeba3fbe73164164b110b
Simon Padbury <simon.padbury@icloud.com>
2018-10-30 13:32:31 +0000

    Adding action hooks

--- single.php
+++ single.php
@@ -1,4 +1,9 @@
-<?php get_header(); ?>
+<?php
+    b4st_action_header_before();
+    get_header(); 
+    b4st_action_header_after();
+    b4st_action_main_before();
+?>
 
 <main class="container mt-5">
   <div class="row">
@@ -9,9 +14,18 @@
       </div><!-- /#content -->
     </div>
 
-    <?php get_sidebar(); ?>
+    <?php 
+      b4st_action_sidebar_before();
+      get_sidebar();
+      b4st_action_sidebar_after();
+    ?>
 
   </div><!-- /.row -->
 </main><!-- /.container -->
 
-<?php get_footer(); ?>
+<?php 
+    b4st_action_main_after();
+    b4st_action_footer_before();
+    get_footer(); 
+    b4st_action_footer_after();
+?>
